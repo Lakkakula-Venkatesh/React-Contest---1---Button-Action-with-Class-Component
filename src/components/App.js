4972a8e2d771759c8478e31d9ef47d93412734ec
Lakkakula-Venkatesh <68702624+Lakkakula-Venkatesh@users.noreply.github.com>
2020-10-21 21:26:29 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,22 @@ class App extends React.Component {
     };
   }
   renderParagraph = () => {
-    this.setState({ para: <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> });
+    this.setState({
+      para: (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )
+    });
   };
   render() {
     return (
       <>
-        <div id="main">{/* Do not remove this main div!! */}</div>
-        <button onClick={this.renderParagraph} id="click"></button>
-        {this.state.para}
+        <div id="main">
+          <button onClick={this.renderParagraph} id="click"></button>
+          {this.state.para}
+        </div>
       </>
     );
   }
