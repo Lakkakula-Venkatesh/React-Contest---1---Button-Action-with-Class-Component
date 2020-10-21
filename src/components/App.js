ea2536a4a6f77ab03c32384600a557f71dea57ed
Lakkakula-Venkatesh <68702624+Lakkakula-Venkatesh@users.noreply.github.com>
2020-10-21 20:45:22 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -10,12 +10,11 @@ class App extends React.Component {
   }
   renderParagraph = () => {
     this.setState({
-      para: (
+      para:
         <p id="para">
           Hello, I've learnt to use the full-stack evaluation tool. This makes
           me so happy
         </p>
-      )
     });
   };
   render() {
