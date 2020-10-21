105bfc042cfc70b14dde50c181916b25bd2d9b8c
Lakkakula-Venkatesh <68702624+Lakkakula-Venkatesh@users.noreply.github.com>
2020-10-21 20:09:47 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,31 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
-
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  constructor(props) {
+    super(props);
+    this.renderParagraph = this.state.renderParagraph;
+  }
+  renderParagraph() {
+    return (
+      <>
+        <p>
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      </>
+    );
+  }
+  render() {
+    return (
+      <>
+        <div id="main">{/* Do not remove this main div!! */}</div>
+        <button onClick={this.state.renderParagraph}>
+          {this.renderParagraph}
+        </button>
+      </>
+    );
+  }
 }
 
-
 export default App;
-
