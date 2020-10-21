40ad24c4475e37250de329911b306b007bc05cf4
Lakkakula-Venkatesh <68702624+Lakkakula-Venkatesh@users.noreply.github.com>
2020-10-21 20:34:55 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,21 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.renderParagraph = this.state.renderParagraph;
+	this.state ={
+		para: (<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy</p>)
+	};
   }
-  renderParagraph() {
-	render() {
-		return (
-			<p id="para">
-			Hello, I've learnt to use the full-stack evaluation tool. This makes
-			me so happy
-			</p>
-		);
-	}
-  }
+  
   render() {
     return (
       <>
         <div id="main">{/* Do not remove this main div!! */}</div>
-        <button onClick={this.state.renderParagraph} id="click"></button>
+        <button onClick={() => this.setState({para: this.state.para})} id="click"></button>
+		{this.state.para}
       </>
     );
   }
