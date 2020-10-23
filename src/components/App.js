7467f5196ed457694841daf654b9f63baae248ea
Lakkakula-Venkatesh <68702624+Lakkakula-Venkatesh@users.noreply.github.com>
2020-10-23 13:29:55 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,56 @@
+// import React, { Component, useState } from "react";
+// import "../styles/App.css";
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       display: false
+//     };
+//     this.renderParagraph = () => {
+//       this.setState({ display: true });
+//     };
+//   }
+//   //
+//   render() {
+//     return (
+//       <>
+//         <div id="main">
+//           <button onClick={this.renderParagraph} id="click"></button>
+//           {this.state.display && (
+//             <p id="para">
+//               "Hello, I've learnt to use the full-stack evaluation tool. This
+//               makes me so happy"
+//             </p>
+//           )}
+//         </div>
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
+
 import React, { Component, useState } from "react";
-import "../styles/App.css";
+// import "../styles/App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      display: false,
       para: ""
     };
     this.renderParagraph = () => {
-      this.setState({ para: (<p id="para">"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"</p>)});
+      this.setState({
+        para: (
+          <p id="para">
+            "Hello, I've learnt to use the full-stack evaluation tool. This
+            makes me so happy"
+          </p>
+        ),
+        display: true
+      });
     };
   }
 
